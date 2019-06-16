832ca78b6eedf3928397e1a92d13826a3f942ad2
aximario <aximario@qq.com>
2019-06-16 19:51:04 +0800

    Add: 增加docs

---
+++ docs/precache-manifest.e21078d96d2aea4dd6c2908cb1e96226.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa7b526eef820dddfa44ed410a592b0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.f0fdb7381e67f460bf91.bundle.js"
+  },
+  {
+    "url": "runtime~main.f0fdb7381e67f460bf91.bundle.js"
+  },
+  {
+    "url": "vendors~main.f0fdb7381e67f460bf91.bundle.js"
+  }
+]);
